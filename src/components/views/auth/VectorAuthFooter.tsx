53eeb52051d2389a402a903f99005ab1fef10a6b
umesh-spirinova <umesh@spirinova.dev>
2023-07-11 15:32:54 +0530

    First commit

---
+++ src/components/views/auth/VectorAuthFooter.tsx
@@ -0,0 +1,46 @@
+/*
+Copyright 2015, 2016 OpenMarket Ltd
+Copyright 2019 New Vector Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import React, { ReactElement } from "react";
+import SdkConfig from "matrix-react-sdk/src/SdkConfig";
+import { _t } from "matrix-react-sdk/src/languageHandler";
+
+const VectorAuthFooter = (): ReactElement => {
+    const brandingConfig = SdkConfig.getObject("branding");
+    const links = brandingConfig?.get("auth_footer_links") ?? [
+    ];
+
+    const authFooterLinks: JSX.Element[] = [];
+    for (const linkEntry of links) {
+        authFooterLinks.push(
+            <a href={linkEntry.url} key={linkEntry.text} target="_blank" rel="noreferrer noopener">
+                {linkEntry.text}
+            </a>,
+        );
+    }
+
+    return (
+        <footer className="mx_AuthFooter" role="contentinfo">
+            {authFooterLinks}
+            <a href="https://matrix.org" target="_blank" rel="noreferrer noopener">
+                {_t("Powered by Matrix")}
+            </a>
+        </footer>
+    );
+};
+
+export default VectorAuthFooter;
